fe4d8d5d3840374c6efc787e6bfc3b2609259637
Thomas Steinbach <thomas.steinbach@aikq.de>
2018-03-13 15:38:12 +0100

    replaced all occurences of 'gitlab-secrets' with 'sogo-secrets'

--- setup-project.sh
+++ setup-project.sh
@@ -3,3 +3,5 @@ sudo oc login -u system:admin
 sudo oc adm policy add-scc-to-user anyuid -z default -n sogo
 oc new-app -f secret-template.yaml
 oc-apply
+oc start-build sogo --from-dir .
+oc start-build sogo-httpd --from-dir .
