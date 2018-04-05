2997d19c9350072d74bb30552618a11ee233b11d
Thomas Steinbach <thomas.steinbach@aikq.de>
2018-04-05 13:12:13 +0200

    switch to project if already existent

--- setup-project.sh
+++ setup-project.sh
@@ -1,4 +1,4 @@
-oc new-project sogo
+oc new-project sogo || oc project sogo
 sudo oc login -u system:admin
 sudo oc adm policy add-scc-to-user anyuid -z default -n sogo
 oc new-app -f secret-template.yaml
