874472355c11d4ffd207f6fff4b263fdd274dc3c
Thomas Steinbach <thomas.steinbach@aikq.de>
2018-04-05 17:15:21 +0200

    added wait times to setup script to not break builds

--- setup-project.sh
+++ setup-project.sh
@@ -3,5 +3,7 @@ sudo oc login -u system:admin
 sudo oc adm policy add-scc-to-user anyuid -z default -n sogo
 oc new-app -f secret-template.yaml
 oc-apply
+sleep 5
 oc start-build sogo --from-dir .
+sleep 120
 oc start-build sogo-httpd --from-dir .
