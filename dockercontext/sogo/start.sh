1b8223da5a92be09eb9568266bd89f8c5acb89d7
Thomas Steinbach <thomas.steinbach@aikq.de>
2018-04-05 16:28:24 +0200

    added memcached and language/timezone defaults

--- dockercontext/sogo/start.sh
+++ dockercontext/sogo/start.sh
@@ -7,6 +7,7 @@ MYSQL_PORT="${SOGO_MYSQL_PORT:-3306}"
 MYSQL_USER="${SOGO_MYSQL_USER:-sogo}"
 MYSQL_PASSWORD="${SOGO_MYSQL_PASSWORD:-sogo}"
 MYSQL_DATABASE="${SOGO_MYSQL_DATABASE:-sogo}"
+MEMCACHED_HOST="${MEMCACHED_HOST:-memcached}"
 
 # configure database settings in maintainance scripts
 sed -i "s#MYSQL_HOST#${MYSQL_HOST}#g" /usr/local/bin/addsogouser /usr/local/bin/delsogouser
@@ -29,6 +30,9 @@ chmod 640 /etc/sogo/sogo.conf
 mysql -h${MYSQL_HOST} -u${MYSQL_USER} -p${MYSQL_PASSWORD} -D${MYSQL_DATABASE} -e "CREATE TABLE IF NOT EXISTS sogo_users (c_uid VARCHAR(10) PRIMARY KEY, c_name VARCHAR(10), c_password VARCHAR(32), c_cn VARCHAR(128), mail VARCHAR(128));"
 
 # sogo server settings
+sudo -u sogo defaults write sogod SOGoLanguage 'German'
+sudo -u sogo defaults write sogod SOGoTimeZone 'Europe/Berlin'
+sudo -u sogo defaults write sogod SOGoMemcachedHost "$MEMCACHED_HOST"
 sudo -u sogo defaults write sogod SOGoUserSources "({canAuthenticate = YES; displayName = \"SOGo Users\"; id = users; isAddressBook = YES; type = sql; userPasswordAlgorithm = md5; viewURL =\"mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}:${MYSQL_PORT}/${MYSQL_DATABASE}/sogo_users\";})"
 sudo -u sogo defaults write sogod SOGoProfileURL "mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}:${MYSQL_PORT}/${MYSQL_DATABASE}/sogo_user_profile"
 sudo -u sogo defaults write sogod OCSFolderInfoURL "mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}:${MYSQL_PORT}/${MYSQL_DATABASE}/sogo_folder_info"
